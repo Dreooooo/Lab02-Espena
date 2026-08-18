c00bebb4dd1a51db3919c83a0901a639894185b7
Andreo <dreoespena01@gmail.com>
2026-08-18 13:26:00 +0800

    Complete Event Planner Activity

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -7,6 +7,27 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+
+            <section className="description">
+                <p>
+                    Plan and organize your events easily with our Event Planner.
+                    Whether it is a personal or corporate event, we are here to help.
+                </p>
+
+                <button className="get-started-button">
+                    Get Started
+                </button>
+            </section>
+
+            <section className="events_categories">
+                <ul>
+                    <h1>Event Categories</h1>
+                    <li>Weddings</li>
+                    <li>Birthday Parties</li>
+                    <li>Corporate Events</li>
+                    <li>Conferences</li>
+                </ul>
+            </section>
         </div>
     );
 };
